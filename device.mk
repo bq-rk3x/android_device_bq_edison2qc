a7cec6e40188f5b6b3af8078bb28e4a91111c47c
Kra1o5 <kra1o5x@gmail.com>
2014-02-07 00:07:31 +0100

    Add module

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/bq/edison2qc/config/media_profiles.xml:system/etc/media_profiles.xml
 
+# Rootdir blob
+PRODUCT_COPY_FILES += \
+    device/bq/edison2qc/rootdir/rk30xxnand.ko.3.0.36+:root/rk30xxnand.ko.3.0.36+
+
 # Default config
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160
