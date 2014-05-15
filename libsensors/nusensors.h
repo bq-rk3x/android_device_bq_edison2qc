d6e21a0721f10fe87ab062a7e43f163cbba1e2b8
J. Carlos Romero <juancarlos.romero@gmail.com>
2014-05-15 14:23:24 +0200

    sensors HAL cleanup

--- libsensors/nusensors.h
+++ libsensors/nusensors.h
@@ -62,35 +62,12 @@ int init_nusensors(hw_module_t const* module, hw_device_t** device);
 #define ID_A	(0)
 #define ID_M	(1)
 #define ID_O	(2)
-#define ID_P  	(3)
-#define ID_L	(4)
-#define ID_GY	(5)
-#define ID_PR	(6)
-#define ID_TMP	(7)
-
-
-/*****************************************************************************/
-
-/*
- * The SENSORS Module
- */
-
-/* the CM3602 is a binary proximity sensor that triggers around 9 cm on
- * this hardware */
-#define PROXIMITY_THRESHOLD_CM  9.0f
 
 /*****************************************************************************/
 
 #define MMA_DEVICE_NAME     GSENSOR_DEV_PATH
 /** akm sensor(M 和 O sensor) 的控制设备文件路径. */
 #define AKM_DEVICE_NAME     "/dev/compass"
-#define PS_DEVICE_NAME      "/dev/psensor"
-#define LS_DEVICE_NAME      "/dev/lightsensor"
-#define GY_DEVICE_NAME      "/dev/gyrosensor"
-#define PR_DEVICE_NAME      "/dev/pressure"
-#define TMP_DEVICE_NAME     "/dev/temperature"
-
-
 
 
 #define EVENT_TYPE_ACCEL_X          ABS_X
@@ -107,19 +84,6 @@ int init_nusensors(hw_module_t const* module, hw_device_t** device);
 #define EVENT_TYPE_MAGV_Y           ABS_HAT0Y
 #define EVENT_TYPE_MAGV_Z           ABS_BRAKE
 #define EVENT_TYPE_MAGV_STATUS      ABS_HAT1X
-
-
-#define EVENT_TYPE_TEMPERATURE      ABS_THROTTLE
-#define EVENT_TYPE_STEP_COUNT       ABS_GAS
-#define EVENT_TYPE_PROXIMITY        ABS_DISTANCE
-#define EVENT_TYPE_LIGHT            ABS_MISC
-
-#define EVENT_TYPE_GYRO_X           REL_RX
-#define EVENT_TYPE_GYRO_Y           REL_RY
-#define EVENT_TYPE_GYRO_Z           REL_RZ
-
-#define EVENT_TYPE_PRESSURE         ABS_PRESSURE
-
 
 
 /** 表征相同的 加速度物理量的时候, Android 上层使用的 数值 和 sensor 数据设备送出的 数值 的 比值. */
@@ -147,13 +111,6 @@ int init_nusensors(hw_module_t const* module, hw_device_t** device);
 #define CONVERT_O_A                 (CONVERT_O)
 #define CONVERT_O_P                 (CONVERT_O)
 #define CONVERT_O_R                 (CONVERT_O)
-
-// conversion of gyro data to SI units (radian/sec)
-#define RANGE_GYRO                  (2000.0f*(float)M_PI/180.0f)
-#define CONVERT_GYRO                ((70.0f / 1000.0f) * ((float)M_PI / 180.0f))
-#define CONVERT_GYRO_X              (CONVERT_GYRO)
-#define CONVERT_GYRO_Y              (CONVERT_GYRO)
-#define CONVERT_GYRO_Z              (CONVERT_GYRO)
 
 #define CONVERT_B                   (1.0f/100.0f)
 
